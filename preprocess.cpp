612aefaeb3659162c2c8c573774e46b7592b4abb
YawarRaza7349 <YawarRaza7349@gmail.com>
2024-05-30 13:43:06 -0700

    Make reading code from input reusable

--- preprocess.cpp
+++ preprocess.cpp
@@ -4,42 +4,55 @@
 #include <iostream>
 
 template<int Dimension, typename T, int N>
-struct table {
+class table {
+private:
     std::array<table<Dimension - 1, T, N>, N> arr;
-    
+
+public:
     template<typename KeyIter>
     auto& operator[](KeyIter&& key_iter) {
-        // create sequence point to ensure deref precedes incr
-        auto& inner = arr[*key_iter];
-        return inner[++key_iter];
+        // create sequence point to ensure dereference precedes increment
+        auto& next = arr[*key_iter];
+        return next[++key_iter];
     }
 };
 
 template<typename T, int N>
-struct table<0, T, N> {
+class table<0, T, N> {
+private:
     T t;
     
+public:
     template<typename KeyIter>
     auto& operator[](KeyIter&&) {
         return t;
     }
 };
 
+template<int L>
+// one extra element for NUL
+class code : public std::array<char, L + 1> {};
+
+template<int L>
+decltype(auto) operator>>(std::istream& input, code<L>& c) {
+    return input.get(c.data(), L + 1);
+}
+
 int main() {
-    auto const code_size = 2;
+    auto const code_length = 2;
     auto const max_code_char = 'Z';
     
     // trade space efficiency for time efficiency
     // by not reindexing code chars to remove gaps
-    table<code_size, std:: string, max_code_char + 1> lookup_table;
-    
-    // one extra element for NUL
-    std::array<char, code_size + 1> code;
+    table<code_length, std::string, max_code_char + 1> lookup_table;
     
     {
         std::ifstream lookup_file("keys.tsv");
-        while (lookup_file.getline(code.data(), code_size + 1, '\t')) {
-            std::getline(lookup_file, lookup_table[code.begin()]);
+        code<code_length> key;
+        
+        while (lookup_file >> key) {
+            lookup_file.getline(nullptr, 0, '\t');
+            std::getline(lookup_file, lookup_table[key.begin()]);
         }
     } // destroy lookup_file
 }
