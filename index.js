c5c66662cb78cdfd1ded548e454119a4f1dd714c
YawarRaza7349 <YawarRaza7349@gmail.com>
2024-06-10 11:30:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ class Observable {
 
 const { startYear: dataStartYear, data: allData } = preprocessed;
 
-const domYearTextNode = document.querySelector("#year").firstChild;
+const domYear = document.querySelector("#year");
 const domYearTextbox = document.querySelector("#year-textbox");
 const domSlider = document.querySelector("#slider");
 const domAnimate = document.querySelector("#animate");
-const domNumStatesTextNode = document.querySelector("#num-states").firstChild;
+const domNumStates = document.querySelector("#num-states");
 const domData = document.querySelector("#data");
 const domDataList = document.querySelector("#data-list");
 const domMapSvg = document.querySelector("#map-svg");
@@ -38,7 +38,7 @@ const year = new Observable();
 
 year.subscribe((y) => {
   const yStr = y.toString();
-  domYearTextNode.nodeValue = yStr;
+  domYear.textContents = yStr;
   domYearTextbox.value = yStr;
 });
 
@@ -105,32 +105,32 @@ domMapSvg.append(...svgContents);
 const fragment = document.createDocumentFragment();
 
 for (const stateData of allData) {
-  stateData.svg = document.getElementById(stateData.state);
-  stateData.textNode = document.createTextNode("");
-  stateData.dlItem = document.createElement("div");
+  const dlItem = document.createElement("div");
   const dt = document.createElement("dt");
   const dd = document.createElement("dd");
   dt.append(stateData.state);
-  dd.append(stateData.textNode);
-  stateData.dlItem.append(dt, dd);
-  fragment.append(stateData.dlItem);
+  dlItem.append(dt, dd);
+  fragment.append(dlItem);
+  stateData.datumElem = dd;
+  stateData.dlItem = dlItem;
+  stateData.svg = document.getElementById(stateData.state);
 }
 
 domDataList.append(fragment);
 
-for (const path of document.querySelectorAll("#map path")) {
+for (const path of document.querySelectorAll("#map-svg path")) {
   path.style.stroke = "black";
 }
 
 year.subscribe((y) => {
   domData.ariaBusy = "true";
-  const numStates = 0;
-  for (const { state, cycle13, cycle17, textNode, dlItem, svg } of allData) {
+  let numStates = 0;
+  for (const { state, cycle13, cycle17, datumElem, dlItem, svg } of allData) {
     const deltaYear = y - dataStartYear;
     const total =
       cycle13[positiveModulo(deltaYear, 13)] +
       cycle17[positiveModulo(deltaYear, 17)];
-    textNode.nodeValue = total.toString();
+    datumElem.textContents = total.toString();
     if (total === 0) {
       dlItem.ariaHidden = "true";
     } else {
@@ -142,7 +142,7 @@ year.subscribe((y) => {
     const blue = Math.trunc(255 - colorValue);
     svg.style.fill = `rgb(255 ${green} ${blue})`;
   }
-  domNumStatesTextNode.nodeValue = numStates.toString();
+  domNumStates.textContents = numStates.toString();
   domData.ariaBusy = "false";
 });
 
